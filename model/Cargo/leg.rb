985b5b85212a5dfcc967ff48ecce6039be329209
Paul Rayner <paul@virtual-genius.com>
2013-05-08 10:05:55 -0600

    Add Leg and RoutingStatus

--- model/Cargo/leg.rb
+++ model/Cargo/leg.rb
@@ -1,19 +1,24 @@
 require 'ice_nine'
 require 'hamster'
 
-class Itinerary
-  attr_reader :legs
-  attr_reader :initial_departure_location
-  attr_reader :final_arrival_location
-  attr_reader :final_arrival_date
+class Leg
+  attr_reader :voyage
+  attr_reader :load_location
+  attr_reader :unload_location
+  attr_reader :load_date
+  attr_reader :unload_date
 
   # TODO Handle empty values for attributes by returning UNKNOWN location
   # TODO Add is_empty method to supporting checking for this in is_empty
 
-  def initialize(legs)
+  def initialize(voyage, load_location, unload_location, load_date, unload_date)
     # TODO Check valid values
 
-    @legs = Hamster.list(legs)
+    @voyage = voyage
+    @load_location = load_location
+    @unload_location = unload_location
+    @load_date = load_date
+    @unload_date = unload_date
 
     IceNine.deep_freeze(self)
   end
---
+++ model/Cargo/routing_status.rb
@@ -0,0 +1,13 @@
+class RoutingStatus
+
+  # Describes status of cargo routing
+  
+  RoutingStatusValues = { 
+              0 => :NotRouted,
+              1 => :Misrouted,
+              2 => :Routed
+            }
+
+  # TODO Figure out how to implement this enum in Ruby, and its type extensions
+
+end
