bd3cbc318bb52e1ecc72b4517eeede4869c3de30
Paul Rayner <paul@virtual-genius.com>
2013-05-21 15:02:47 -0600

    Add links to reference source .NET implementation for Cargo

--- model/Cargo/cargo.rb
+++ model/Cargo/cargo.rb
@@ -16,18 +16,21 @@ class Cargo
     # @delivery = Delivery.derived_from(@route_specification, @itinerary)
   end
 
+  # cf. https://github.com/SzymonPobiega/DDDSample.Net/blob/master/DDDSample-Vanilla/Domain/Cargo/Cargo.cs#L55
   def specify_new_route (route_specification)
     # TODO: add exception checking for invalid (null) values
     @route_specification = route_specification
     # Delivery.update_on_routing(@route_specification, @itinerary)
   end
 
+  # cf. https://github.com/SzymonPobiega/DDDSample.Net/blob/master/DDDSample-Vanilla/Domain/Cargo/Cargo.cs#L69
   def assign_to_route (itinerary)
     # TODO: add exception checking for invalid (null) values
     @itinerary = itinerary
     # Delivery.update_on_routing(@route_specification, @itinerary)
   end
 
+  # cf. https://github.com/SzymonPobiega/DDDSample.Net/blob/master/DDDSample-Vanilla/Domain/Cargo/Cargo.cs#L83
   def derive_delivery_progress (last_handling_event)
     # @delivery = Delivery.derived_from(last_handling_event)
   end
