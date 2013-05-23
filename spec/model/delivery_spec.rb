40403fe74007f8e45167ae8a110b52b2f6f04cc3
Paul Rayner <paul@virtual-genius.com>
2013-05-23 16:42:15 -0600

    Create failing specs for Delivery.

--- spec/model/delivery_spec.rb
+++ spec/model/delivery_spec.rb
@@ -1,5 +1,7 @@
+require 'spec_helper'
 require 'rspec'
 require 'date'
+
 # TODO Improve the way model requires are working
 require "#{File.dirname(__FILE__)}/../../model/cargo/cargo"
 require "#{File.dirname(__FILE__)}/../../model/cargo/leg"
@@ -11,5 +13,19 @@ require "#{File.dirname(__FILE__)}/../../model/location/unlocode"
 
 # TODO Implement delivery specs
 describe "Delivery" do
+  it "Cargo is not considered unloaded at destination if there are no recorded handling events" do
+    false.should == true
+  end
 
+  it "Cargo is not considered unloaded at destination after handling unload event but not at destination" do
+    false.should == true
+  end
+
+  it "Cargo is not considered unloaded at destination after handling other event at destination" do
+    false.should == true
+  end
+
+  it "Cargo is considered unloaded at destination after handling unload event at destination" do
+    false.should == true
+  end
 end
