6403d4999998567eddb950b7cc1a46af0c912830
Dan Sharp <Dan.Sharp@irco.com>
2013-05-29 17:55:24 -0600

    All required models required via single support helper (for integration specs)

---
+++ spec/support/models_require.rb
@@ -0,0 +1,14 @@
+# helper file to require all models
+require 'cargo'
+require 'leg'
+require 'itinerary'
+require 'tracking_id'
+require 'route_specification'
+require 'location'
+require 'unlocode'
+require 'delivery'
+require 'handling_activity'
+require 'handling_event'
+require 'handling_event_type'
+
+require 'date'
