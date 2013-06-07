3a6110c1e04e14c349868fed013510c9219d8f94
Paul Rayner <paul@virtual-genius.com>
2013-06-07 17:19:05 -0600

    Make cargo inspection service subscription global to application.

--- config/initializers/loader.rb
+++ config/initializers/loader.rb
@@ -11,6 +11,5 @@ Dir.glob("#{Rails.root.to_s}/ports/**/*.rb").each do |f|
 end
 
 # TODO Put here for now, but needs to be in a better place
-# puts "Subscribing cargo inspection service to registering handling events..."
-# register_handling_event = RegisterHandlingEvent.new
-# register_handling_event.add_listener(CargoInspectionService.new, :async => true)
+puts "Subscribing cargo inspection service to handling event registrations..."
+Wisper::GlobalListeners.add_listener(CargoInspectionService.new, :async => true)
