8acd4f4d57d957aefc419334e9d908a5d526e293
Paul Rayner <paul@virtual-genius.com>
2013-06-07 17:52:00 -0600

    Publish async events when cargo is misdirected or unloaded at destination.

--- app/controllers/cargo_inspection_service.rb
+++ app/controllers/cargo_inspection_service.rb
@@ -1,10 +1,22 @@
 class CargoInspectionService
+  include Wisper::Publisher
+
   def cargo_was_handled(tracking_id, last_handling_event)
     cargo_repository = CargoRepository.new
     cargo = cargo_repository.find_by_tracking_id(tracking_id)
-    puts "Found cargo ", cargo.inspect
-    puts "Updating based on last handling event ", last_handling_event.inspect
+    puts "Old delivery ", cargo.delivery.inspect
     cargo.derive_delivery_progress(last_handling_event)
+    puts "New delivery ", cargo.delivery.inspect
+    publish(:cargo_is_misdirected, tracking_id) if cargo.delivery.is_misdirected 
+    publish(:cargo_is_unloaded_at_destination, tracking_id) if cargo.delivery.is_unloaded_at_destination
     cargo_repository.store(cargo)
+  end
+
+  def cargo_is_misdirected(tracking_id)
+    puts "Cargo is misdirected - need to reroute it! ", tracking_id.inspect
+  end
+
+  def is_unloaded_at_destination(tracking_id)
+    puts "Cargo has arrived at the destination - notify the customer.", tracking_id.inspect
   end
 end
