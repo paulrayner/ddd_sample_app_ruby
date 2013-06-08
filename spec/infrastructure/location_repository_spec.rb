eb31d72a5b989078aedb30e94a9e783005fdcbbc
Paul Rayner <paul@virtual-genius.com>
2013-06-07 22:15:34 -0600

    Initialize locations with hash list.

--- spec/infrastructure/location_repository_spec.rb
+++ spec/infrastructure/location_repository_spec.rb
@@ -15,4 +15,34 @@ describe "LocationRepository" do
     found_location = location_repository.find(UnLocode.new('HKG'))
     found_location.should == location
   end
+
+  it "Location can be stored and then found" do
+
+    locations = {
+                    'USCHI' => 'Chicago',
+                    'USDAL' => 'Dallas',
+                    # 'SEGOT' => 'Göteborg',
+                    'DEHAM' => 'Hamburg',
+                    'CNHGH' => 'Hangzhou',
+                    'FIHEL' => 'Helsinki',
+                    'CNHKG' => 'Hongkong',
+                    'AUMEL' => 'Melbourne',
+                    'USNYC' => 'New York',
+                    'NLRTM' => 'Rotterdam',
+                    'CNSHA' => 'Shanghai',
+                    'SESTO' => 'Stockholm',
+                    'JNTKO' => 'Tokyo'
+                 }
+    location_repository = LocationRepository.new
+
+    # TODO Replace this quick-and-dirty data teardown...
+    location_repository.nuke_all_locations
+
+    locations.each do | code, name |
+        location_repository.store(Location.new(UnLocode.new(code), name))
+    end
+
+    found_locations = location_repository.find_all()
+    found_locations.size.should == 10
+  end
 end
