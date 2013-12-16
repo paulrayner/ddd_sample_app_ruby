4f14fa3104910336c765b8c28e3135641faca593
Mark Maglana <mmaglana@gmail.com>
2013-12-16 09:48:02 -0800

    Use Guard
    This will eliminate the need to alt-tab between editor and terminal
    just to run the tests and see the results.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,6 @@ RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
   config.color_enabled = true
-  config.formatter = :documentation
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
